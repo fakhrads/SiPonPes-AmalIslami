b3b2abe1b415cf3f0dd0d5650dc4e7b44a077636
fakhrads <fakhriadisaputra15@gmail.com>
2023-01-23 22:15:19 +0700

    Add seeder

--- database/seeders/User.ts
+++ database/seeders/User.ts
@@ -8,19 +8,19 @@ export default class extends BaseSeeder {
         nama: 'Fakhri Adi Saputra',
         email: 'fakhrads.id@gmail.com',
         password: 'secret',
-        level: "superadmin",
+        level: "Operator",
       },
       {
         nama: 'Fauzi Fikrinullah',
         email: 'fauzifikrinullah@gmail.com',
         password: 'secret',
-        level: "admin",
+        level: "TU",
       },
       {
         nama: 'Ji Kun',
         email: 'jikun@gmail.com',
         password: 'secret',
-        level: "admin",
+        level: "TU",
       },
     ])
   }
